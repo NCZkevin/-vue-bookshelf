1b17d04659d4465f999ffffd2c9c5520ae0f2192
nczkevin <z1376871863@163.com>
2016-07-04 22:09:15 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import WildVue from "wildvue"
+import Wildog from 'wildog'
 
+Vue.use(Wildog)
 Vue.use(VueResource)
 Vue.use(VueRouter)
 /* eslint-disable no-new */
